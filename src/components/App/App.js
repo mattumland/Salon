0285fadede61b836d6ae07b5d48937cb3dcc0371
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-10 17:17:05 -0600

    Fetch object ids, set state of ids

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [ wall, setWall ] = useState([]);
+  const [ ids, setIDs ] = useState({});
+  const [ error, setError ] = useState('');
+  // const [ artDetail, setArtDetail ] = useState({});
+  // const [ favorites, setFavorites ] = useState([]);
+  //const [ searchTerms, setSearchTerms ] = useState([]);
+
+  const getIDs = async () => {
+    const idPath = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers';
+    setError('');
+
+    try {
+      const response = await fetch(idPath);
+      const responseIDs = await response.json();
+      setIDs(responseIDs);
+    } catch (error) {
+      setError(error)
+    }
+  }
+
+  useEffect(() => {
+    getIDs();
+  }, [])
+
   return (
     <div className="App">
-
+      {console.log(ids)}
     </div>
   );
 }
