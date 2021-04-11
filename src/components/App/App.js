75ffe13106c47e235d5efe17d58e18f83782a6f2
Matt Umland <mattumland@gmail.com>
2021-04-11 17:29:51 -0400

    Add basic salon template without formatting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,38 +12,38 @@ function App() {
   // const [ favorites, setFavorites ] = useState([]);
   //const [ searchTerms, setSearchTerms ] = useState([]);
 
-  const artIdSearch = fetch('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers')
-    .then(response => response.json())
-    .catch(error => setError(error.message))
-
-
-  const getIDs = async () => {
-    const idMatch = await artIdSearch;
-    setError('');
-    setIDs(idMatch.objectIDs)
-  }
-
-
-  const getSingleArtPiece = async () => {
-    const randomIndex = getRandomIndex(ids);
-    const itemStock = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${ids[randomIndex]}`)
-      .then(response => response.json());
-
-    try {
-      const artPiece = await itemStock;
-      setWall(wall => [...wall, artPiece]);
-    } catch (error) {
-      setError(error)
-    }
-  }
-
-  useEffect(() => {
-    getIDs()
-  }, [])
-
-  useEffect(() => {
-    ids && getSingleArtPiece();
-  }, [ids])
+  // const artIdSearch = fetch('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers')
+  //   .then(response => response.json())
+  //   .catch(error => setError(error.message))
+  //
+  //
+  // const getIDs = async () => {
+  //   const idMatch = await artIdSearch;
+  //   setError('');
+  //   setIDs(idMatch.objectIDs)
+  // }
+  //
+  //
+  // const getSingleArtPiece = async () => {
+  //   const randomIndex = getRandomIndex(ids);
+  //   const itemStock = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${ids[randomIndex]}`)
+  //     .then(response => response.json());
+  //
+  //   try {
+  //     const artPiece = await itemStock;
+  //     setWall(wall => [...wall, artPiece]);
+  //   } catch (error) {
+  //     setError(error)
+  //   }
+  // }
+  //
+  // useEffect(() => {
+  //   getIDs()
+  // }, [])
+  //
+  // useEffect(() => {
+  //   ids && getSingleArtPiece();
+  // }, [ids])
 
   return (
     <div className="App">
--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Artwork.scss';
+import '../Wall/Wall.scss';
 
 const Artwork = ({ id, url, wallLocation }) => {
 
   return (
-    <div data={id} className="wallLocation">
-      <img src=`{url}`>
+    <div data={id} className={wallLocation}>
+      <img src={url} />
     </div>
   )
 
