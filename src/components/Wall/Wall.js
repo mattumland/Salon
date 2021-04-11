32efb5a6805fbb2035550660ce4077a8a75b840f
Matt Umland <mattumland@gmail.com>
2021-04-11 14:44:58 -0400

    Add dummy data

---
+++ src/components/Wall/Wall.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Wall.scss';
+
+const Wall = () => {
+
+  const artwork = [
+    {objectID: 468350, primaryImageSmall: "https://images.metmuseum.org/CRDImages/md/web-large/DP154235.jpg"},
+    {objectID: 206989, primaryImageSmall: "https://images.metmuseum.org/CRDImages/es/web-large/DT8910.jpg"},
+    {objectID: 436102, primaryImageSmall: "https://images.metmuseum.org/CRDImages/ep/web-large/DP-1410-001.jpg"},
+    {objectID: 816522, primaryImageSmall: "https://images.metmuseum.org/CRDImages/ep/web-large/DP-1410-001.jpg"},
+    {objectID: 2032, primaryImageSmall: "https://images.metmuseum.org/CRDImages/ad/web-large/85I_ACF3100R5.jpg"},
+    {objectID: 20141, primaryImageSmall: "https://images.metmuseum.org/CRDImages/ad/web-large/L.2009.22.35.jpg"},
+    {objectID: 2019, primaryImageSmall: "https://images.metmuseum.org/CRDImages/ad/web-large/85I_ACF3093R6.jpg"},
+    {objectID: 208554, primaryImageSmall: "https://images.metmuseum.org/CRDImages/es/web-large/DT4036.jpg"}
+  ]
+
+  return (
+      
+  )
+
+}
+
+export default Wall;
