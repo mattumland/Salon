2ac4a406fd047a92f2159ffe4ce9de9144c40a5b
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-12 16:00:14 -0600

    Restructure details sections, remove comments

--- src/components/ArtDetails/ArtDetails.js
+++ src/components/ArtDetails/ArtDetails.js
@@ -4,8 +4,6 @@ import './ArtDetails.scss';
 const ArtDetails = () => {
   const [ selectedArt, setSelectedArt ] = useState('');
 
-  //getSingleArtPiece(artPieceID)
-    //.then setSelectedArt(repsonse)
     const getSingleArtPiece = async () => {
       const singleArtURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/437329';
 
@@ -13,7 +11,6 @@ const ArtDetails = () => {
         const response = await fetch(singleArtURL);
         const artPiece = await response.json();
         setSelectedArt(artPiece);
-        // await console.log(selectedArt)
       } catch (error) {
         console.log(error)
       }
@@ -24,16 +21,19 @@ const ArtDetails = () => {
     }, [])
 
   return (
-    <section className="art-details">
-      {console.log(selectedArt)}
-      <img  src={selectedArt.primaryImage} alt={selectedArt.title}/>
-      <caption>
-        <p>"{selectedArt.title}"</p>
-        <p>{selectedArt.objectBeginDate}-{selectedArt.objectEndDate}</p>
-        <p>{selectedArt.artistDisplayName}</p>
-        <p>Medium: {selectedArt.medium}</p>
-      </caption>
-    </section>
+    <>
+      <section className="art-details">
+        {console.log(selectedArt)}
+        <img  src={selectedArt.primaryImage} alt={selectedArt.title}/>
+        <aside>
+          <h2>Featured Artifact:</h2>
+          <h3>"{selectedArt.title}"</h3>
+          <p>c. {selectedArt.objectBeginDate}-{selectedArt.objectEndDate}</p>
+          <p>{selectedArt.artistDisplayName}</p>
+          <p>{selectedArt.medium}</p>
+        </aside>
+      </section>
+    </>
   )
 }
 
