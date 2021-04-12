5b77785149da1b49f81d6d50fe3bec7c1bc7c008
percworld <percworld@gmail.com>
2021-04-11 22:11:15 -0600

    Add shuffle art

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-import { getRandomIndex } from '../../utilities.js';
+import { shuffleItems } from '../../utilities.js';
 
 function App() {
   const [wall, setWall] = useState([]);
@@ -19,15 +19,13 @@ function App() {
   const getIDs = async () => {
     const idMatch = await artIdSearch;
     setError('');
-    setIDs(idMatch.objectIDs)
+    setIDs(idMatch.objectIDs);
   }
 
 
-  const getSingleArtPiece = async () => {
-    const randomIndex = getRandomIndex(ids);
-    const itemStock = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${ids[randomIndex]}`)
+  const getSingleArtPiece = async (index) => {
+    const itemStock = fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${index}`)
       .then(response => response.json());
-
     try {
       const artPiece = await itemStock;
       setWall(wall => [...wall, artPiece]);
@@ -37,18 +35,26 @@ function App() {
   }
 
   useEffect(() => {
-    getIDs()
+    getIDs();
   }, [])
 
   useEffect(() => {
-    ids && getSingleArtPiece();
+    ids.length && getSingleArtPiece(shuffleItems(ids)[0]);
+    ids.length && getSingleArtPiece(shuffleItems(ids)[1]);
+    ids.length && getSingleArtPiece(shuffleItems(ids)[2]);
+    ids.length && getSingleArtPiece(shuffleItems(ids)[3]);
+    ids.length && getSingleArtPiece(shuffleItems(ids)[4]);
   }, [ids])
 
   return (
-    <div className="App">
-      {/* {ids.length && console.log('IDs: ', ids)} */}
-      {/* {wall.length && console.log('WALL: ', wall)} */}
-      {wall.length > 1 && <img src={wall[1].primaryImageSmall} />}
+    <div className="App">Salon
+      {/* {ids.length && console.log('Rendering IDs: ', ids)}
+      {wall.length && console.log('WALL: ', wall)} */}
+      {wall.length && <img src={wall[0].primaryImageSmall} />}
+      {wall[1] && <img src={wall[1].primaryImageSmall} />}
+      {wall[2] && <img src={wall[2].primaryImageSmall} />}
+      {wall[3] && <img src={wall[3].primaryImageSmall} />}
+      {wall[4] && <img src={wall[4].primaryImageSmall} />}
     </div>
   );
 }
--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,20 @@
 export const getRandomIndex = (arr) => {
   return Math.floor(Math.random() * arr.length);
 }
+
+export const shuffleItems = array => {
+  // const idList = arr.reduce((acc, index) => {
+  //   console.log(acc)
+  //   // if (!acc.includes(index)) {
+  //   acc.push(Math.floor(Math.random() * arr.length))
+  //   // }
+  // }, [])
+  // return idList;
+
+  // using Array sort and Math.random
+
+  return array.sort(() => 0.5 - Math.random());
+
+}
+
+
