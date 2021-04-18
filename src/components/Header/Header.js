5aee89b4bf9ac83028bc9c3fa4e8fe6d39484511
Matt Umland <mattumland@gmail.com>
2021-04-18 16:40:02 -0400

    Add page reload function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,20 +4,23 @@ import './Header.scss';
 
 const Header = () => {
 
+  const refresh = () => {
+    location.reload();
+  }
+
   return (
     <header>
       <Link
         to='/'>
-        <button className="logo-container">
-          <h1>Salon</h1>
-        </button>
+        <button className="logo-container"><h1>Salon</h1></button>
       </Link>
-      <Link
-        to='/favorites'>
-        <button className="user-buttons">
-            <h2>favorites</h2>
-        </button>
-      </Link>
+      <section className="user-buttons">
+        <button onClick={refresh}><h2>see a new wall</h2></button>
+        <Link
+          to='/favorites'>
+          <button><h2>favorites</h2></button>
+        </Link>
+      </section>
     </header>
   )
 }
