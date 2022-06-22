1a40b3f5833445184a2841af0abfb8a7080080aa
Matt Umland <mattumland@gmail.com>
2022-06-22 14:01:49 -0400

    Update npm packages

--- src/components/Wall/Wall.js
+++ src/components/Wall/Wall.js
@@ -22,7 +22,7 @@ const Wall = () => {
 
   return (
       <>
-        {(state.wallDisplay.length < 7) && (
+        {(state.wallDisplay.length < artworkToDisplay.length) && (
           <h3 className='loading'> Your display is being curated. </h3>
         )}
 
